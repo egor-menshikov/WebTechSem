fb215eea5b154d791d7c5ab9e11c39e9c59eedf6
Egor Menshikov <r3born.fs@gmail.com>
2023-06-09 19:49:14 +0300

    sem_2 edit

---
+++ Sem_2/hello.js
@@ -0,0 +1,12 @@
+// let m = prompt('input');
+// let n = parseInt(m);
+// alert(`Hi, ${n + 10}`);
+// let numbers = (n != 7) ? 'yes seven' : 'no not seven';
+// alert(numbers);
+
+let n = 5;
+let m = 6;
+function add(a, b) {
+    return a + b;
+}
+alert(add(n, m));
